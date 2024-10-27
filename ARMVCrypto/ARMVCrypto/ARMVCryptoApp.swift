f961e75f1590cbbd666d5a3716ba8b79efef5c92
Abhishek Verma <abhishek.rmv@gmail.com>
2024-10-27 23:32:46 +0530

    Initial Project with Assets and colors

---
+++ ARMVCrypto/ARMVCrypto/ARMVCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARMVCryptoApp.swift
+//  ARMVCrypto
+//
+//  Created by Abhishek on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ARMVCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARMVCrypto/ARMVCrypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARMVCrypto
+//
+//  Created by Abhishek on 27/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
