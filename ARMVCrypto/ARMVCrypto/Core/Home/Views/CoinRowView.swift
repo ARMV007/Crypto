51f644d1f745499e9542253c7a715c4d240c4d67
Abhishek Verma <abhishek.rmv@gmail.com>
2024-10-28 02:52:30 +0530

    add CoinRowView
    add CoinRowView
    add DeveloperPreview helper
    add extend Double for Currency formatting

---
+++ ARMVCrypto/ARMVCrypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,75 @@
+//
+//  CoinRowView.swift
+//  ARMVCrypto
+//
+//  Created by Abhishek on 28/10/24.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: CoinModel
+    let showHoldingColumn: Bool
+    
+    var body: some View {
+        
+        HStack(spacing: 0) {
+            leftColumn
+            Spacer()
+            if showHoldingColumn {
+                centerColumn
+            }
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+#Preview {
+    Group {
+        CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingColumn: true)
+        CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingColumn: true)
+            .preferredColorScheme(.dark)
+    }
+    
+}
+
+
+extension CoinRowView {
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(.cryptoSecondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol)
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundStyle(.cryptoAccent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundStyle(.cryptoAccent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing){
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundStyle(.cryptoAccent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundStyle(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ? .cryptoGreen : .cryptoRed
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+}
